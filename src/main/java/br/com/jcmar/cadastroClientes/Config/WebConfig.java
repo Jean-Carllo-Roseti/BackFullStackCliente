26d459d1b47e68ac3b7de36616bf534661c1ea80
jean carllo <jeanrosete04@gmail.com>
2024-09-12 18:04:15 -0300

    realizada a permissão de todos os verbos e cabeçalhos.

--- src/main/java/br/com/jcmar/cadastroClientes/Config/WebConfig.java
+++ src/main/java/br/com/jcmar/cadastroClientes/Config/WebConfig.java
@@ -10,6 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Ajuste conforme necessário
+                .allowedHeaders("*"); // Permite todos os cabeçalhos
+
     }
 }
